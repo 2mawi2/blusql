736e3a0bbb657ca990eaaa1d8886b4a26f26b711
Kramer, Jan (DEV) <jan.kramer@materna.group>
2025-07-01 17:08:39 +0200

    extracting database version and change from hardcoded schema and database version to file import

--- blusql/skill/stubs.py
+++ blusql/skill/stubs.py
@@ -20,44 +20,48 @@ class DbContext(BaseModel):
 class DbContextProvider:
     
     def get_schema(self) -> DbContext:
-        schema = """
-        CREATE TABLE employee (
-    Employee_ID  INT,
-    PRIMARY KEY (Employee_ID),
-    Name         TEXT,
-    Age          INT,
-    City         TEXT,
-    CONSTRAINT sqlite_autoindex_employee_1 UNIQUE (Employee_ID)
-);
-CREATE TABLE evaluation (
-    Employee_ID   TEXT,
-    Year_awarded  TEXT,
-    Bonus         REAL,
-    CONSTRAINT sqlite_autoindex_evaluation_1 UNIQUE (Employee_ID, Year_awarded),
-    FOREIGN KEY (Employee_ID) REFERENCES employee (Employee_ID),
-    PRIMARY KEY (Employee_ID, Year_awarded)
-);
-CREATE TABLE hiring (
-    Shop_ID       INT,
-    Employee_ID   INT,
-    PRIMARY KEY (Employee_ID),
-    Start_from    TEXT,
-    Is_full_time  bool,
-    CONSTRAINT sqlite_autoindex_hiring_1 UNIQUE (Employee_ID),
-    FOREIGN KEY (Employee_ID) REFERENCES employee (Employee_ID),
-    FOREIGN KEY (Shop_ID) REFERENCES shop (Shop_ID)
-);
-CREATE TABLE shop (
-    Shop_ID          INT,
-    PRIMARY KEY (Shop_ID),
-    Name             TEXT,
-    Location         TEXT,
-    District         TEXT,
-    Number_products  INT,
-    Manager_name     TEXT,
-    CONSTRAINT sqlite_autoindex_shop_1 UNIQUE (Shop_ID)
-);"""
-        db_technology = "PostgreSQL Version 10"
+#         schema = """
+#         CREATE TABLE employee (
+#     Employee_ID  INT,
+#     PRIMARY KEY (Employee_ID),
+#     Name         TEXT,
+#     Age          INT,
+#     City         TEXT,
+#     CONSTRAINT sqlite_autoindex_employee_1 UNIQUE (Employee_ID)
+# );
+# CREATE TABLE evaluation (
+#     Employee_ID   TEXT,
+#     Year_awarded  TEXT,
+#     Bonus         REAL,
+#     CONSTRAINT sqlite_autoindex_evaluation_1 UNIQUE (Employee_ID, Year_awarded),
+#     FOREIGN KEY (Employee_ID) REFERENCES employee (Employee_ID),
+#     PRIMARY KEY (Employee_ID, Year_awarded)
+# );
+# CREATE TABLE hiring (
+#     Shop_ID       INT,
+#     Employee_ID   INT,
+#     PRIMARY KEY (Employee_ID),
+#     Start_from    TEXT,
+#     Is_full_time  bool,
+#     CONSTRAINT sqlite_autoindex_hiring_1 UNIQUE (Employee_ID),
+#     FOREIGN KEY (Employee_ID) REFERENCES employee (Employee_ID),
+#     FOREIGN KEY (Shop_ID) REFERENCES shop (Shop_ID)
+# );
+# CREATE TABLE shop (
+#     Shop_ID          INT,
+#     PRIMARY KEY (Shop_ID),
+#     Name             TEXT,
+#     Location         TEXT,
+#     District         TEXT,
+#     Number_products  INT,
+#     Manager_name     TEXT,
+#     CONSTRAINT sqlite_autoindex_shop_1 UNIQUE (Shop_ID)
+# );"""
+#         db_technology = "PostgreSQL Version 10"
+        with open("schema.json", "r") as f:
+            schema = f.read()
+        with open("db_technology.txt", "r") as f:
+            db_technology = f.read()
         return DbContext(
             schema=schema,
             db_technology=db_technology
